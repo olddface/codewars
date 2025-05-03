2a1d0d121011ca23ad8f4d34f48fee8651770ec7
olddfacem <alangxyz123@gmail.com>
2025-05-03 20:11:18 +0700

    finish isbn 10

--- isbn-10.py
+++ isbn-10.py
@@ -1,12 +1,28 @@
 def valid_ISBN10(isbn):
     index = 0
     remainder = 0
+    countChara = 0
+    if len(isbn) != 10:
+        return False
     for i in isbn:
         index += 1
-        i = int(i)
-        remainder += index * i
+        if i.isdigit():
+            i = int(i)
+            remainder += index * i
+        else:
+            countChara += 1
+            if i.lower() == "x" and index == 10:
+                remainder += index * 10
+            elif i.lower() == "x" and index != 10:
+                return False
+            else:
+                remainder += index * 0
+
+        print(i," * ",index, " = ", index * i, "\n", "total = ", remainder, "\n\n")
+    if countChara == 10:
+        return False
     remainder = remainder % 11
-    return remainder
+    return remainder == 0
 
 jon = valid_ISBN10('048665088X')
 print(jon)
